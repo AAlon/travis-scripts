aef74a1bb71ff47e7f3dda2ed845187d1067b9fa
Tim Robinson <tim@amazon.com>
2019-04-09 09:21:39 -0700

    Testing pushing a new version of the file using CodeCommit API

--- before_deploy.sh
+++ before_deploy.sh
@@ -10,4 +10,6 @@ fi
 # Fetch the relevant S3 bucket & CodePipeline
 export S3_BUCKET_NAME=`aws s3 ls | grep "travis-source" | awk '{print $3}'`
 export SA_NAME_WITHOUT_DASHES=`echo $SA_NAME | sed -e 's/-//g'`
-export CC_REPO_CLONE_URL_HTTP=`aws codecommit get-repository --repository-name AppManifest-"$SA_NAME"-"$ROS_DISTRO"-gazebo"$GAZEBO_VERSION" | jq -r '.repositoryMetadata | .cloneUrlHttp'`
+export APP_MANIFEST_REPO="AppManifest-$SA_NAME-$ROS_DISTRO-gazebo$GAZEBO_VERSION"
+echo "App manifest repo: $APP_MANIFEST_REPO"
+export CC_REPO_CLONE_URL_HTTP=`aws codecommit get-repository --repository-name $APP_MANIFEST_REPO | jq -r '.repositoryMetadata | .cloneUrlHttp'`
--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -7,6 +7,22 @@ if [ ! -z "${TRAVIS_TAG}" ]; then
     exit 0
 fi
 
+
+# Get the HEAD commit ID for version.json in master branch if exists
+BRANCH_INFO=`aws codecommit get-branch --repository-name $CC_REPO_NAME --branch-name mainline`
+if [ $? -ne 0 ]; then
+    echo "Could not find mainline branch for repository $CC_REPO_NAME. Creating first commit."
+else
+    export BRANCH_COMMIT_ID=`echo $BRANCH_INFO | jq -r '.branch.commitId'`
+fi
+
+export PARENT_COMMIT_FLAG=""
+if [ -z "$BRANCH_COMMIT_ID" ]; then
+    PARENT_COMMIT_FLAG="--parent-commit-id=$BRANCH_COMMIT_ID"
+
+aws codecommit put-file --repository-name "$CC_REPO_NAME" --branch-name mainline --file-content '{"application_version": "1.0.0.1"}' --file-path "/version.json" --commit-message "Bumping version" --name "$GH_USER_NAME" --email "$GH_USER_EMAIL" $PARENT_COMMIT_FLAG
+
+
 # Move artifacts to shared/<version>/ and version.json to shared/
 mkdir $SA_VERSION && mv $TRAVIS_BUILD_DIR/shared/* $SA_VERSION && mv $SA_VERSION $TRAVIS_BUILD_DIR/shared/
 cp "$TRAVIS_BUILD_DIR/version.json" "$TRAVIS_BUILD_DIR/shared/version.json"
@@ -16,6 +32,8 @@ ls $TRAVIS_BUILD_DIR
 echo -----
 ls "$TRAVIS_BUILD_DIR/shared"
 echo here
+
+exit 0
 # Clone the CC repository where version.json lives and commit the version.json file produced by current build
 git clone "$CC_REPO_CLONE_URL_HTTP" cc-repo-for-version-file
 cd cc-repo-for-version-file
