c9e2016fbe6c9ce50d59eb477afbff222be5e624
AAlon <avishaya@amazon.com>
2019-06-17 15:33:03 -0700

    Install latest colcon-bundle in robot_ws

--- ws_builds/robot_ws.sh
+++ ws_builds/robot_ws.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+COLCON_BUNDLE_INSTALL_PATH="${HOME}/colcon-bundle"
+rm -rf "${COLCON_BUNDLE_INSTALL_PATH}"
+git clone https://github.com/colcon/colcon-bundle "${COLCON_BUNDLE_INSTALL_PATH}"
+pip3 install --upgrade pip
+pip install -U --editable "${COLCON_BUNDLE_INSTALL_PATH}"
+
 rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
