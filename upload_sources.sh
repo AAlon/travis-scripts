126a5475243792628e72b6bac1934e2095a82fe0
AAlon <avishaya@amazon.com>
2019-10-03 14:22:55 -0700

    Update upload_sources.sh

--- upload_sources.sh
+++ upload_sources.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
-# This script is meant to be run from Travis directly, outside of a docker container.
+!/bin/bash
+# This is a standalone script that is meant to be run from Travis directly, outside of a docker container.
 set -xe
 
-apt-get update
+sudo apt-get update
 if [ "${ROS_VERSION}" == "1" ]; then
-  apt-get install -y python-rosinstall
+  sudo apt-get install -y python-rosinstall
 elif [ "${ROS_VERSION}" == "2" ]; then
-  apt-get install -y python3-rosinstall
+  sudo apt-get install -y python3-rosinstall
 else
   echo "ROS_VERSION not defined or recognized"
   exit 1
+fi
+
+
+if [ -z "$WORKSPACES" ]; then
+  WORKSPACES="robot_ws simulation_ws"
 fi
 
 for WS in $WORKSPACES
@@ -23,5 +28,6 @@ do
 done
 
 SOURCES_INCLUDES="${WORKSPACES} LICENSE* NOTICE* README* roboMakerSettings.json"
+mkdir shared 2>/dev/null
 /usr/bin/zip -r shared/sources.zip $SOURCES_INCLUDES
 tar cvzf shared/sources.tar.gz $SOURCES_INCLUDES
